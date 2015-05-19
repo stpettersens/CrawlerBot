e960ba8c57fe64a5506a960a7d9fbf0c986c5186
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-05-19 02:07:29 +0100

    Good progress.

--- CrawlerBot.py
+++ CrawlerBot.py
@@ -24,17 +24,19 @@ import xml.dom.minidom as xml
 # This class handles the crawling itself.
 class CrawlerBot:
 
-	disallowed = []
-	links = []
+	disallowed = [] # Disallowed links.
+	links = [] # Allowed links.
 
+	nofollow = False # Do not follow a link.
 	verbose = False # Not verbose by default.
 	robots_str = 'CrawlerBot' # User agent string as used in a robots.txt user agent directive.
 	ua_str = 'Mozilla/5.0 (compatible; CrawlerBot/1.0; +https://github.com/stpettersens/crawlerbot)' 
 	headers = { 'User-Agent': ua_str }
 	# UA string as used in HTTP requests.
-	website = 'http://www.something.com' # Dummy value for website to crawl; will be overriden on invocation.
+	website = '' # Website to crawl; will be overriden on invocation.
+	current = '' # Current page to crawl; will be overriden on invocation.
 
-	cache = 'cache.db'
+	cache = 'cache.db' # Filename to use for caching database.
 
 	# Cache link to SQLite database file.
 	@staticmethod
@@ -64,6 +66,8 @@ class CrawlerBot:
 		if os.path.isfile(CrawlerBot.cache): os.remove(CrawlerBot.cache)
 
 		CrawlerBot.website = site
+		CrawlerBot.current = site
+		CrawlerBot.links.append(site)
 		
 		request = urllib2.Request(site + '/robots.txt', None, CrawlerBot.headers)
 		robots = urllib2.urlopen(request).read()
@@ -72,12 +76,16 @@ class CrawlerBot:
 
 		request = urllib2.Request(site, None, CrawlerBot.headers)
 		html = urllib2.urlopen(request).read()
-		parser = MetaLinkParser()
+		parser = MetaParser()
 		parser.feed(html.lstrip())
 
-		sorted_links = self.trimLinks(CrawlerBot.links, CrawlerBot.disallowed)
-		for link in sorted_links:
-			self.followLink(link)
+		if CrawlerBot.nofollow == False:
+			parser = LinkParser()
+			parser.feed(html.lstrip())
+
+			sorted_links = self.trimLinks(CrawlerBot.links, CrawlerBot.disallowed)
+			for link in sorted_links:
+				self.followLink(link)
 
 		if db:
 			if out == None: out = 'links.db'
@@ -92,21 +100,28 @@ class CrawlerBot:
 		f_sorted_links = []
 		for link in sorted_links:
 			if link in disallowed:
-				pass
+				pass 
 			else:
 				f_sorted_links.append(link)
 		return f_sorted_links
 
 	# Follow a link
 	def followLink(self, link):
-		if CrawlerBot.verbose:
-			print('----------------------------------------------------------------')
-			print('Following link ---> {0}'.format(link))
-			print('----------------------------------------------------------------')
+		CrawlerBot.current = link
+
 		request = urllib2.Request(link, None, CrawlerBot.headers)
 		html = urllib2.urlopen(request).read()
-		parser = MetaLinkParser()
+		parser = MetaParser()
 		parser.feed(html.lstrip())
+
+		if CrawlerBot.nofollow == False:
+			parser = LinkParser()
+			parser.feed(html.lstrip())
+
+			if CrawlerBot.verbose:
+				print('----------------------------------------------------------------')
+				print('Following link ---> {0}'.format(link))
+				print('----------------------------------------------------------------')
 
 	# Write list of links to a SQLite database file.
 	def writeToDatabase(self, links, disallowed, out):
@@ -130,13 +145,9 @@ class CrawlerBot:
 		timestamp = re.sub(' ', 'T', str(datetime.datetime.now()))
 		timestamp = re.sub('\.\d{6}', '+00:00', timestamp)
 
-		if len(links) > 0:
-			sitemap = xml.Document()
-			urlset = sitemap.createElement('urlset')
-			sitemap.appendChild(urlset)
-		elif CrawlerBot.verbose:
-			print('No links to generate sitemap for, terminating...')
-			sys.exit(0)
+		sitemap = xml.Document()
+		urlset = sitemap.createElement('urlset')
+		sitemap.appendChild(urlset)
 
 		for link in sorted_links:
 			url = sitemap.createElement('url')
@@ -158,24 +169,24 @@ class CrawlerBot:
 			priority_is = sitemap.createTextNode('0.8')
 			priority.appendChild(priority_is)
 
-		f = codecs.open(out, 'w', 'utf-8-sig')
-		f.write(sitemap.toprettyxml(encoding='utf-8'))
-		f.close()
+		if len(CrawlerBot.links) > 1:
+			f = codecs.open(out, 'w', 'utf-8-sig')
+			f.write(sitemap.toprettyxml(encoding='utf-8'))
+			f.close()
 
-		f = codecs.open(out, 'r', 'utf-8-sig')
-		lines = f.readlines()
-		f.close()
-		lines[1] = re.sub('\<urlset\>', '<urlset ' + xmlns, lines[1])
+			f = codecs.open(out, 'r', 'utf-8-sig')
+			lines = f.readlines()
+			f.close()
+			lines[1] = re.sub('\<urlset\>', '<urlset ' + xmlns, lines[1])
 		
-		f = codecs.open(out, 'w', 'utf-8-sig')
-		for line in lines:
-			f.write(line)
-		f.close()
+			f = codecs.open(out, 'w', 'utf-8-sig')
+			for line in lines:
+				f.write(line)
+			f.close()
 
 # Parse robots.txt file for a site.
 class RobotsParser():
 
-	disallowed = []
 	ua_match = '({0}|\*)'.format(CrawlerBot.robots_str)
 
 	def feed(self, robots_file):
@@ -214,7 +225,7 @@ class RobotsParser():
 			elif line.startswith('Allow:'):
 				allow_pair = line.split()
 				if CrawlerBot.verbose:
-					print('I am allowed ({0}-ed) to scan: {1}'.format(allow_pair[0][:-1], allow_pair[1])) 
+					print('I am allowed ({0}-ed) to scan: {1}'.format(allow_pair[0][:-1], allow_pair[1]))
 
 			elif line.startswith('Disallow:'):
 					forbidden_pair = line.split()
@@ -228,31 +239,58 @@ class RobotsParser():
 						if CrawlerBot.verbose:
 							print('I am forbidden from crawling {0}.\nI will comply.'.format(CrawlerBot.website))
 							print('Terminating...')
-						sys.exit(-1)
+						sys.exit(1)
 
-# Parse internal links for a site [a(href="http://site.xyz/link.ext")].
-class MetaLinkParser(HTMLParser):
+# Parse metadata for a site.
+class MetaParser(HTMLParser):
 
 	def handle_starttag(self, tag, attrs):
+		tag = tag.lower() # Treat all tags as lowercase.
 
-		if tag == 'a':
-			print(attrs)
+		if tag == 'meta':
+			robots = False
+			for attr in attrs:
+				if attr[0] == 'name' or attr[0] == 'NAME':
+					if attr[1] == 'robots' or attr[1] == 'ROBOTS':
+						robots = True
+				elif attr[0] == 'content' or attr[0] == 'CONTENT':
+					if robots:
+						if re.search('nofollow', attr[1], re.IGNORECASE):
+							if CrawlerBot.verbose:
+								print('I am not following links on this page as directed by META element...')
 
-		# Do not store nofollow links.
-		#if tag == 'a' and attrs[1][0] == 'rel' or tag == 'a' and attrs[2][0] == 'rel':
-			#if attrs[1][0] == 'nofollow' or attrs[2][0] == 'nofollow':
-				#if(CrawlerBot.verbose):
-					#print('Skipping \'nofollow\' link: {0}'.format(attr[0][1]))
+							CrawlerBot.nofollow = True
+
+						if re.search('noindex', attr[1], re.IGNORECASE):
+							if CrawlerBot.verbose:
+								print('I will not index this page as directed by META element...')
+
+							CrawlerBot.disallowed.append(CrawlerBot.current)
+
+# Parse internal links for a site [a(href="http://site.xyz/link.ext")].
+class LinkParser(HTMLParser):
+
+	def handle_starttag(self, tag, attrs):
+		tag = tag.lower() # Treat all tags as lowercase.
 
 		# Store normal internal links.
-		elif tag == 'a' and attrs[0][0] == 'href':
-			print(attrs)
+		if tag == 'a' and attrs[0][0] == 'href':
 			link = attrs[0][1]
+			for attr in attrs:
+				if attr[0] == 'rel' and attr[1] == 'nofollow':
+					CrawlerBot.nofollow = True
+
 			full = re.search('^https?://|#', link)	# Identify out-bound (external) links and ignore them.
-			if full:
+			if full or CrawlerBot.nofollow:
+				if CrawlerBot.verbose:
+					if full == None: link = '{0}/{1}'.format(CrawlerBot.website, link).lstrip()
+					link = re.sub('\/index\.\w{3,4}', '', link)
+					print('Ignoring external, \'nofollow\' or # link: {0}'.format(link))
 				pass
+
 			else:
 				link = '{0}/{1}'.format(CrawlerBot.website, link).lstrip()
+				link = re.sub('\/index\.\w{3,4}', '', link)
 				link = re.sub('/{2}', '/', link)
 				link = re.sub('p:/', 'p://', link)
 				if CrawlerBot.verbose:
